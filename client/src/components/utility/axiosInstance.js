0fe607236f243074fe97345ce0c4c19820b8c097
Freeman <pfreejohnson@gmail.com>
2022-07-29 02:02:04 +0200

    Update server

--- client/src/components/utility/axiosInstance.js
+++ client/src/components/utility/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosApiInstance = axios.create();
 
-axiosApiInstance.defaults.baseURL = 'http://localhost:3001';
+// axiosApiInstance.defaults.baseURL = 'http://localhost:3001';
 axiosApiInstance.defaults.withCredentials = true;
 
 export default axiosApiInstance;
